0d290cfff5fe8d0a258e0570aea5c252f01fcb32
hands0meX <1258849260@qq.com>
2024-03-15 18:12:50 +0800

    feat: 添加ar设备slam的数据的支持记录图片和位置接口的功能

--- fast_server.py
+++ fast_server.py
@@ -1,5 +1,7 @@
 import argparse
+import json
 import os
+import time
 from fastapi import FastAPI, Request
 import numpy as np
 import cv2
@@ -43,6 +45,47 @@ async def match(request: Request, w: int, h: int, dataset: str):
 
     return HLocalizer.detect(dataset)
 
+
+@app.post("/sfm/")
+async def match(request: Request, w: int, h: int, dataset: str, pos: str):
+    if not dataset:
+        return {"message": "dataset is required"}
+
+    sum_buffer = await request.body()
+    width = w
+    height = h
+    print(pos,'pos')
+
+    # 切分 ybuffer 和 uvbuffer
+    ybuffer = sum_buffer[:width * height]
+    uvbuffer = sum_buffer[width * height:]
+
+    # 将 ybuffer 和 uvbuffer 转换为 NumPy 数组
+    ybuffer_np = np.frombuffer(ybuffer, dtype=np.uint8)
+    uvbuffer_np = np.frombuffer(uvbuffer, dtype=np.uint8)
+
+    # 重新构造 YUV 图像
+    yuv_image = np.concatenate((ybuffer_np.reshape((height, width)),
+                                uvbuffer_np.reshape((height // 2, width))),
+                                axis=0)
+
+    gray_image = cv2.cvtColor(yuv_image, cv2.COLOR_YUV2GRAY_420)
+    gray_rotated = cv2.rotate(gray_image, cv2.ROTATE_90_CLOCKWISE)
+
+    folder_path = f"datasets/{dataset}/mapping"
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+
+    timestemp = int(time.time())
+
+    output_file = f"{folder_path}/frame_{timestemp}.jpg"
+    cv2.imwrite(output_file, gray_rotated)
+
+     # 在写入图片之后，追加一条pos的记录
+    with open(f'datasets/{dataset}/pos.txt', 'a') as f:
+        f.write(f"frame_{timestemp} {pos}" + '\n')
+
+    return {"msg": "done"}
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
--- py_demo.py
+++ py_demo.py
@@ -2,6 +2,7 @@ import os
 import shutil
 import pycolmap
 import pathlib
+import numpy as np
 
 # # 官方提取的方法
 # output_path: pathlib.Path = pathlib.Path("outputs/desk/sfm")
@@ -20,7 +21,6 @@ import pathlib
 
 
 # hloc方法
-import pycolmap
 # print(dir(pycolmap))
 # print(pycolmap.has_cuda)
 
@@ -39,11 +39,11 @@ reconstruction = pycolmap.Reconstruction(output_path)
 for image_id, image in reconstruction.images.items():
     # image.name = os.path.basename(image.name)
     # image.name = "mapping/" + image.name
-    image.abc = "foo"
     # print(image_id, dir(image))
     # print(image_id, image.summary())
     # print(image_id, image)
     # print(image_id, image.points2D)
+    print(image.name, image.cam_from_world.translation)
 
 # for point3D_id, point3D in reconstruction.points3D.items():
 #     print(point3D_id, point3D)
@@ -54,4 +54,24 @@ for image_id, image in reconstruction.images.items():
     # print(camera_id, camera.params)
     # print(camera_id, camera.params_to_string())
 
-reconstruction.write(output_path)
+# reconstruction.write(output_path)
+
+# # 创建一个空的列表来存储cam_from_world.translation的值和对应的image name
+# translations = []
+# names = []
+
+# for image_id, image in reconstruction.images.items():
+#     translations.append(image.cam_from_world.translation)
+#     names.append(image.name)
+
+# # 计算所有的直线距离，并存储对应的image name
+# distances = {}
+# for i in range(len(translations)):
+#     for j in range(i+1, len(translations)):
+#         dist = np.linalg.norm(translations[i] - translations[j])
+#         distances[(names[i], names[j])] = dist
+
+# # 将结果存储在一个txt文本中
+# with open('distances.txt', 'w') as f:
+#     for names, dist in distances.items():
+#         f.write(f'{names[0]} {names[1]} {dist}\n')
